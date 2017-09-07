c0e89a4e7c796cf514d14557566f901c0f00b1cc
lichi.chen[陳立其] <lichi.chen@104.com.tw>
2017-09-07 17:55:27 +0800

    add function find, update, and delete by id

--- api/controllers/company.js
+++ api/controllers/company.js
@@ -1,4 +1,5 @@
 var companyInfo = require('../helpers/companyInfo');
+var ObjectID = require('mongodb').ObjectID;
 
 function insertOne(req, res) {
   var doc = req.swagger.params.body.value;
@@ -11,8 +12,50 @@ function find(req, res) {
     res.json(doc);
   });
 }
-
+function findOneById(req, res) {
+  var id = req.swagger.params._id.value;
+  if (!isMongoId(id)) {
+    res.status(400)
+      .json({
+        message: 'get a invalid mongoID.'
+      });
+  }
+  companyInfo.findOneDocument(new ObjectID(id), function(doc) {
+    res.json(doc);
+  });
+}
+function isMongoId(id) {
+  return /^[a-f0-9]{24}$/i.test(id);
+}
+function updateOneById(req, res) {
+  var id = req.swagger.params._id.value;
+  if (!isMongoId(id)) {
+    res.status(400)
+      .json({
+        message: 'Receive a invalid mongoID'
+      });
+  }
+  var update = req.swagger.params.body.value;
+  companyInfo.updateOneDocument(new ObjectID(id), update, function(doc) {
+    res.json(doc);
+  });
+}
+function deleteOneById(req, res) {
+  var id = req.swagger.params._id.value;
+  if (!isMongoId(id)) {
+    res.status(400).json({
+      message: 'Receive a invalid mongoID'
+    })
+  }
+  companyInfo.deleteOneDocument(new ObjectID(id), function(doc) {
+    doc ? res.json(doc) : res.json({});
+    
+  })
+}
 module.exports = {
   insertOne,
-  find
+  find,
+  findOneById,
+  updateOneById,
+  deleteOneById
 };
