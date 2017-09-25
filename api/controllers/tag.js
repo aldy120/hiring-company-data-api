445772ebca968b992217ccd6c91b0e3e776c3825
lichi.chen <lichi.chen@lichichende-MacBook-Pro.local>
2017-09-25 11:50:39 +0800

    Add createTad and FindTag
    createTag use the companyID and tagName to create a new tag (or use the old existed tag if the name was used), and then add the tagID to the tags field of company.
    
    findTag return a tag list contains all tags of the given company.

---
+++ api/controllers/tag.js
@@ -0,0 +1,64 @@
+var companyInfo = require('../helpers/companyInfo');
+var tagCol = require('../helpers/tagCol');
+var ObjectID = require('mongodb').ObjectID;
+
+function isMongoID(id) {
+  return /^[a-f0-9]{24}$/i.test(id);
+}
+function createTag(req, res) {
+  var companyId = req.swagger.params._id.value;
+  if (!isMongoID(companyId)) {
+    res.status(400).json({message: 'not a valid mongo ID'});
+    return;
+  }
+  var tag = req.swagger.params.body.value;
+  // check company exist
+  companyInfo.findOneDocument(new ObjectID(companyId), function(company) {
+    // company do not exist 
+    if (!company) {
+      res.status(404).json({message: 'company not found'});
+      return;
+    }
+
+    // check tag name is not used
+    tagCol.findOneDocument(tag, function(doc) {
+      // if tag do not exist
+      if (!doc) {
+        tagCol.insertOneDocument(tag);
+        doc = tag;
+      }
+      
+      // doc is the tag
+      companyInfo.addToTags(new ObjectID(companyId), doc._id, function(r) {
+        res.json({
+          add: doc,
+          to: r.value
+        });
+      });
+    })
+
+  })
+}
+
+function findTags(req, res) {
+  var companyID = req.swagger.params._id.value;
+  if (!isMongoID(companyID)) {
+    res.status(400).json({message: 'not a valid mongo ID'});
+    return;
+  }
+  companyInfo.findOneDocument(new ObjectID(companyID), function(doc) {
+    if (!doc) {
+      res.status(404).json({message: 'company not found'});
+      return;
+    }
+    companyInfo.lookupTags(new ObjectID(companyID), function(docs) {
+      res.json(docs.map(doc => doc.tag_docs[0]));
+    })
+  })
+  
+}
+
+module.exports = {
+  createTag,
+  findTags
+}
